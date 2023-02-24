523300f608c2d08f53d82f8f3b4c9d8d717682ec
wjdals95 <wjdals992@naver.com>
2023-02-24 16:21:59 +0900

    라우터 페이지 애니메이션

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+
 import Layout from "./layout/layout";
-import Home from './pages/Home';
+import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import ProjectDetail from "./components/ProjectDetail";
 
+import './App.css'
 
 function App() {
+  const location = useLocation();
   return (
     <Layout>
-      <Switch>
-      <Route path='/' exact>
-            <Home />
-        </Route>
-        <Route path='/about'>
-            <About />
-        </Route>
-        <Route path='/projects' exact>
-            <Projects />
-        </Route>
-        <Route path='/projects/projectDetail1'>
-          <ProjectDetail />
-        </Route>
-      </Switch>
+      <TransitionGroup className="transitions-wrapper">
+        <CSSTransition
+          key={location.pathname}
+          classNames={"right"}
+          timeout={300}
+        >
+          <Switch location={location}>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/projects" exact>
+              <Projects />
+            </Route>
+            <Route path="/projects/projectDetail1">
+              <ProjectDetail />
+            </Route>
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
     </Layout>
   );
 }
